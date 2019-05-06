d81d0e97c3e36a0f5d7df8d3cee163bd99c009ad
DannyCorbett89 <dannycorbett890@gmail.com>
2019-05-06 14:06:44 +0100

    Switched mount page to use react-data-grid

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import NavBar from './NavBar'
 import Videos, {VideoButtons} from './videos'
 import Minions from './minions'
 import Info, {InfoButtons} from './info'
-import TableCell from "@material-ui/core/TableCell";
-import {withStyles} from "@material-ui/core";
 import Button from "@material-ui/core/Button/Button";
 import Dialog from "@material-ui/core/Dialog/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle/DialogTitle";
@@ -16,61 +14,16 @@ import DialogContent from "@material-ui/core/DialogContent/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions/DialogActions";
 import Select from "@material-ui/core/Select/Select";
 import MenuItem from "@material-ui/core/MenuItem/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel/InputLabel";
 import Paper from "@material-ui/core/Paper/Paper";
 import {isMobile} from "react-device-detect";
 import LinearProgress from "@material-ui/core/LinearProgress/LinearProgress";
-import Table from "@material-ui/core/Table";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import TableBody from "@material-ui/core/TableBody";
 import DialogContentText from "@material-ui/core/DialogContentText/DialogContentText";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem/ListItem";
 import Checkbox from "@material-ui/core/Checkbox/Checkbox";
 import ListItemText from "@material-ui/core/ListItemText/ListItemText";
 import Avatar from "@material-ui/core/Avatar/Avatar";
-
-class MessageWindow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: props.text,
-            onClick: props.onClick
-        };
-    }
-
-    render() {
-        // Render nothing if the "show" prop is false
-        if (!this.props.show) {
-            return null;
-        }
-
-        let button;
-
-        if (this.state.onClick != null) {
-            button = <DialogActions>
-                <Button onClick={this.state.onClick} color="primary">
-                    Close
-                </Button>
-            </DialogActions>;
-        } else {
-            button = <div/>;
-        }
-
-        return (
-            <Dialog
-                open="true"
-                onClose={this.handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">{this.state.text}</DialogTitle>
-                {button}
-            </Dialog>
-        );
-    }
-}
+import ReactDataGrid from "react-data-grid";
 
 class AddPlayer extends React.Component {
     constructor(props) {
@@ -317,121 +270,6 @@ class RemovePlayerButton2 extends React.Component {
     }
 }
 
-class AddMount extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-            mounts: [],
-            mountName: ''
-        };
-        this.handleMountChange = this.handleMountChange.bind(this);
-    }
-
-    componentWillMount() {
-        fetch(backendUrl + '/mounts/available')
-            .then(results => {
-                return results.json();
-            })
-            .then(data => {
-                this.setState({
-                    mounts: data,
-                    mountName: data[0].name
-                });
-                console.log("listAvailableMounts", this.state.mounts);
-            })
-            .catch(error => {
-                console.log("Empty response for /listAvailableMounts", error)
-            });
-    }
-
-    handleMountChange(event) {
-        this.setState({
-            mountName: event.target.value
-        });
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleClickOpen = () => {
-        this.setState({open: true});
-    };
-
-    handleClose = () => {
-        this.setState({open: false});
-    };
-
-    render() {
-        let window;
-
-        if (this.state.mountName === "") {
-            window = <MessageWindow show={this.state.open} text={"No more mounts to add"} onClick={this.handleClose}/>;
-        } else {
-            window = <Dialog
-                open={this.state.open}
-                onClose={this.handleClose}
-                aria-labelledby="form-dialog-title"
-            >
-                <DialogTitle id="form-dialog-title">Add Mount</DialogTitle>
-                <DialogContent>
-                    <InputLabel>Mount&nbsp;</InputLabel>
-                    <Select onChange={this.handleMountChange}
-                            value={this.state.mountName}>
-                        {this.state.mounts.map((mount) =>
-                            <MenuItem key={mount.name} value={mount.name}>{mount.name}</MenuItem>
-                        )}
-                    </Select>
-                </DialogContent>
-                <DialogActions>
-                    <AddMountButton mountName={this.state.mountName}/>
-                    <Button onClick={this.handleClose} color="primary">
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>;
-        }
-
-        return (
-            <div className="titleButton">
-                <Button color="inherit" onClick={this.handleClickOpen}>Add Mount</Button>
-                {window}
-            </div>
-        );
-    }
-}
-
-class AddMountButton extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            disabled: false,
-            text: "Add"
-        };
-    }
-
-    addMount() {
-        this.setState({
-            disabled: true,
-            text: "Adding Mount..."
-        });
-        fetch(backendUrl + "/mounts/add?name=" + this.props.mountName, {method: 'POST'})
-            .then(() => {
-                window.location.reload();
-            });
-    }
-
-    render() {
-        return (
-            <Button id={this.props.mountName}
-                    color="primary"
-                    onClick={() => this.addMount(this.props.mountName)}
-                    disabled={this.state.disabled}>{this.state.text}</Button>
-        );
-    }
-}
-
 class Ranks extends React.Component {
     constructor(props) {
         super(props);
@@ -550,94 +388,13 @@ class ApplyRanksButton extends React.Component {
     }
 }
 
-class RemovePlayerButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            disabled: false,
-            text: "Remove",
-            player: props.player,
-            messageWindowIsOpen: false
-        };
-    }
-
-    removePlayer(name) {
-        this.setState({
-            disabled: true,
-            messageWindowIsOpen: true
-        });
-        fetch(backendUrl + "/players/remove?name=" + name, {method: 'DELETE'})
-            .then(() => {
-                window.location.reload();
-            });
-    }
-
-    render() {
-        return (
-            <div>
-                <Button id={this.state.player.name}
-                        variant="outlined"
-                        size="small"
-                        onClick={() => this.removePlayer(this.state.player.name)}
-                        disabled={this.state.disabled}>{this.state.text}</Button>
-                <MessageWindow show={this.state.messageWindowIsOpen} text={"Removing " + this.state.player.name}/>
-            </div>
-        );
-    }
-}
-
-class RemoveMountButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            disabled: false,
-            text: "Remove",
-            mount: props.mount,
-            messageWindowIsOpen: false
-        };
-    }
-
-    removeMount(mount) {
-        this.setState({
-            disabled: true,
-            messageWindowIsOpen: true
-        });
-        fetch(backendUrl + "/mounts/remove?id=" + mount.id, {method: 'DELETE'})
-            .then(() => {
-                window.location.reload();
-            });
-    }
-
-    render() {
-        return (
-            <div>
-                <Button id={this.state.mount.name}
-                        variant="outlined"
-                        size="small"
-                        onClick={() => this.removeMount(this.state.mount)}
-                        disabled={this.state.disabled}>{this.state.text}</Button>
-                <MessageWindow show={this.state.messageWindowIsOpen} text={"Removing " + this.state.mount.name}/>
-            </div>
-        );
-    }
-}
-
-const CustomTableCell = withStyles(theme => ({
-    head: {
-        textAlign: 'center'
-    },
-    body: {
-        textAlign: 'center'
-    },
-}))(TableCell);
-
 class Mounts extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            updated: null,
-            columns: null,
-            players: [{mounts: []}]
+            columns: [],
+            players: [{mounts: []}],
+            numRows: 0
         };
     }
 
@@ -650,8 +407,9 @@ class Mounts extends React.Component {
             .then(data => {
                 this.setState({
                     updated: data.lastUpdated,
-                    columns: data.numColumns,
-                    players: data.players
+                    columns: data.columns,
+                    players: data.players,
+                    numRows: data.numRows
                 });
                 console.log("listMounts", this.state.players);
             })
@@ -663,45 +421,17 @@ class Mounts extends React.Component {
         let updated;
 
         if (this.state.updated == null) {
-            table = <div>
+            table = <div className="maincontent">
                 <p align="center">Loading mounts...</p>
                 <LinearProgress/>
             </div>
         } else if (this.state.players.length === 0) {
             table = <p align="center">No Players are being tracked</p>
         } else {
-            table = <Table padding="none">
-                <TableHead>
-                    <TableRow>
-                        <CustomTableCell>Name</CustomTableCell>
-                        <CustomTableCell colSpan={this.state.columns + 1}>Mounts
-                            Needed</CustomTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {this.state.players.map((player) =>
-                        <TableRow key={player.name} className="highlight">
-                            <CustomTableCell>{player.name}</CustomTableCell>
-
-                            {player.mounts.map((mount) =>
-                                <CustomTableCell key={mount.id}>{mount.instance}</CustomTableCell>
-                            )}
-
-                            <CustomTableCell>
-                                <RemovePlayerButton player={player}/>
-                            </CustomTableCell>
-                        </TableRow>)}
-                    <TableRow>
-                        <CustomTableCell/>
-                        {this.state.players[0].mounts.map((mount) =>
-                            <CustomTableCell key={mount.id} align="center">
-                                <RemoveMountButton mount={mount}/>
-                            </CustomTableCell>
-                        )}
-                        <CustomTableCell/>
-                    </TableRow>
-                </TableBody>
-            </Table>;
+            table = <ReactDataGrid columns={this.state.columns}
+                                   rowGetter={i => this.state.players[i]}
+                                   rowsCount={this.state.numRows}
+                                   enableCellSelect={true}/>;
 
             updated = <p>Last Updated: {this.state.updated}</p>;
         }
@@ -718,6 +448,7 @@ class Mounts extends React.Component {
             result =
                 <div>
                     {table}
+                    <br/>
                     {updated}
                 </div>;
         }
@@ -735,7 +466,7 @@ class MountButtons extends React.Component {
         return (
             <div className="titleButtons">
                 <AddPlayer/>
-                <AddMount/>
+                <RemovePlayer/>
                 <Ranks/>
             </div>
         );
